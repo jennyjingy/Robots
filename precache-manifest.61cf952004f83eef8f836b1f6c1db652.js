10bf23b5eb9486257741b46a305ec29c1bd23c0a
Jenny <jingyaohui11@gmail.com>
2019-12-06 22:28:09 -0500

    Updates

---
+++ precache-manifest.61cf952004f83eef8f836b1f6c1db652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67308895fc2eb9d112ba6493ac80149",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "3600d7d45b9139cee256",
+    "url": "/Robots/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "9000c816a3257bc4613e",
+    "url": "/Robots/static/css/main.5743c44d.chunk.css"
+  },
+  {
+    "revision": "3600d7d45b9139cee256",
+    "url": "/Robots/static/js/2.d5b71d16.chunk.js"
+  },
+  {
+    "revision": "9000c816a3257bc4613e",
+    "url": "/Robots/static/js/main.55dd091b.chunk.js"
+  },
+  {
+    "revision": "ed9f7d55b5a9f6249d05",
+    "url": "/Robots/static/js/runtime-main.56a8f18a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
